2f480989098b12b6d1f398e5921c6b2de97adc29
xiaozhang <862449030@qq.com>
2020-08-28 11:38:50 +0800

    blog

--- footer.php
+++
@@ -1,7 +0,0 @@
-<div id="footer-wrapper">
-	<div id="footer">
-		<p>Copyright © 2030<a href=" <?php echo get_option('home'); ?>"> <?php bloginfo('name'); ?></a>.</p>
-	</div>
-    <?php wp_footer(); ?>
-	<!-- end #footer -->
-</div>
--- index.php
+++ index.php
@@ -1,37 +1,13 @@
-<?php get_header();?>
-	<!-- end #menu -->
-	<div id="page">
-	<div id="page-bgtop">
-	<div id="page-bgbtm">
-		<div id="content">
-        <?php if (have_posts()) : ?>
-<?php while (have_posts()) : the_post(); ?>
+<?php
+/**
+ * 前端页面加载
+ * @copyright (c) Emlog All Rights Reserved
+ */
 
-			<div class="post">
-				<h2 class="title"><a href="<?php the_permalink() ?>"> <?php echo mb_strimwidth(get_the_title(), 0, 32, ''); ?></a></h2>
-				<p class="meta">发布日期：<?php the_time('Y-m-d'); ?>  - 分类：<?php the_category(', ') ?>  - <?php the_tags('标签： ', ', ', ''); ?> - 评论：<?php comments_number('暂无评论', '1条评论', '% 评论' );?></p>
-				<div class="entry">
-					<p><?php echo mb_strimwidth(strip_tags(apply_filters('the_content', $post->post_content)), 0, 200,"……"); ?></p>
-					
-					<p><a href="<?php the_permalink() ?>" class="links">更多内容</a> </p>
-				</div>
-			</div>
-			<?php endwhile; ?>
-            <?php if(function_exists('wp_page_numbers')) : wp_page_numbers(); endif; ?>
-<?php endif;?>
+require_once 'init.php';
 
-			
-		<div style="clear: both;">&nbsp;</div>
-		</div>
-		<!-- end #content -->
-		<?php get_sidebar();?>
-		<!-- end #sidebar -->
-		<div style="clear: both;">&nbsp;</div>
-	</div>
-	</div>
-	</div>
-	<!-- end #page -->
-</div>
-<?php get_footer();?>
-</body>
-</html>
+define('TEMPLATE_PATH', TPLS_PATH.Option::get('nonce_templet').'/');//前台模板路径
+
+$emDispatcher = Dispatcher::getInstance();
+$emDispatcher->dispatch();
+View::output();
